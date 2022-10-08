3f77ba39a7fb60130a4e00f8eb3baf5ecaf18677
DevelopJKong <ybaram40@gmail.com>
2022-10-08 21:19:42 +0900

    #3.2 Babel 완료

---
+++ server.js
@@ -0,0 +1,20 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen(5000).then(() => console.log('http://localhost:5000'));
