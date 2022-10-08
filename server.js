d75161d8c2668b7bd332d2f97f5e1e74932afa23
DevelopJKong <ybaram40@gmail.com>
2022-10-08 22:05:21 +0900

    #3.3 POC API 완료

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
+
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String, year: Int): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'world',
+    movies: () => [],
+    movie: () => ({ title: 'Hello', year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, args) => {
+      console.log(args);
+      return true;
+    },
+    deleteMovie: (_, args) => {
+      console.log(args);
+      return true;
+    },
   },
 };
 
