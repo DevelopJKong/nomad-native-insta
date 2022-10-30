8480b5626413b7f7b27dacae141379f8ca26531c
DevelopJKong <ybaram40@gmail.com>
2022-10-30 22:57:47 +0900

    #3.12 Recap 완료

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+import "dotenv/config";
 import { ApolloServer } from 'apollo-server';
 import schema from './schema';
 const server = new ApolloServer({
   schema,
 });
 
-server.listen(5000).then(() => console.log('http://localhost:5000'));
+const PORT = process.env.PORT;
+
+
+server.listen(PORT).then(() => console.log('http://localhost:5000'));
